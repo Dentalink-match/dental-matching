bd7af8bd6facc06586241b5dcf88ca7c9e82886b
Dentalink-match <bilkisnopur@gmail.com>
2025-10-03 23:55:03 +0600

    customSupabaseClient.js
    remove sensitive file

--- src/lib/customSupabaseClient.js
+++ src/lib/customSupabaseClient.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://wfxjhmmdxsjyhstxjipf.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmeGpobW1keHNqeWhzdHhqaXBmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0NDAyNjMsImV4cCI6MjA3NDAxNjI2M30.Ymw7_TI99qIhSqC8z5uKzhLo3mK8unl9cJUCwMyv43Y';
-
+const supabaseUrl = 
+const supabaseAnonKey = 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
